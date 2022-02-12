81eb5976e31986662ac02e5a4b2c8489cbfffecc
Josca <jonathanscharff@gmail.com>
2022-02-12 12:47:50 +0100

    Removed redundant scripts

--- scripts/crawl_cyberconnect.py
+++
@@ -1,6 +0,0 @@
-from veritas import crawlers, io
-from pathlib import Path
-
-mirror_data = Path("data/mirror")
-mirror = crawlers.Mirror(wallet_fp=arweave_wallet_fp)
-cursor = "WyIyMDIyLTAyLTExVDA3OjEzOjQ0LjI3OVoiLDFd"
